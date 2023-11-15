184d6aedc2965278320020b77a884c55d6c3cc3d
brucema94 <49718928+brucema94@users.noreply.github.com>
2023-11-15 03:16:45 +0100

    Add files via upload

--- Test.py
+++ Test.py
@@ -3,18 +3,41 @@ import matplotlib.pyplot as plt
 from scipy import interpolate
 import pandas as pd
 
-loaded_data = np.load('C:/Users/bruce/Desktop/Compfi/std_tenor_eur3m.npy', allow_pickle=True)
+loaded_data_discount = np.load('C:/Users/bruce/Desktop/Compfi/std_tenor_eonia.npy', allow_pickle=True)
+loaded_data_forward = np.load('C:/Users/bruce/Desktop/Compfi/std_tenor_eur3m.npy', allow_pickle=True)
 
-Test = loaded_data[:20]
-tenors = Test[:, 1]
-interest_rates = Test[:, 2]
-Discount_factors = np.zeros(len(interest_rates))
+# Define a function for linear interpolation
+def linear_interpolation(x, x_values, y_values):
+    f = interpolate.interp1d(x_values, y_values, kind='linear')
+    return f(x)
 
-for i in range(len(tenors)):
-    Discount_factors[i] = 1/ (1+interest_rates[i] * tenors[i])
+dt = 0.001
+maturity=10
+num_intervals = int(maturity / dt)
 
-# 0.0023111894406597226 = 0.23111894406597226 Annualize fixed rate
-Par_Swap_rate = (1- Discount_factors[-1])/ sum(Discount_factors)
+Intervals = np.arange(0.0, maturity+dt, dt)
 
-print(Par_Swap_rate)
+# taking a single day entry as our t0
+Test_discount = loaded_data_discount[:20]
+Test_forward = loaded_data_forward[:20]
 
+# split columns for interpolation 
+dates = Test_discount[:, 0]
+tenors = Test_discount[:, 1]
+interest_rates_discount = Test_discount[:, 2]
+interest_rates_forward = Test_forward[:, 2]
+#tenors = loaded_data_forward[:, 1]
+
+# discount rate interpolation 
+interpolated_rates_discount = [linear_interpolation(interval, tenors, interest_rates_discount) for interval in Intervals]
+result_list = list(zip(Intervals, interpolated_rates_discount))
+result_list_float = [(interval, float(rate)) for interval, rate in result_list]
+Discount_factor_M = [(t, np.exp(-r * t)) for t, r in result_list_float]
+
+interpolated_rates_forward = [linear_interpolation(interval, tenors, interest_rates_forward) for interval in Intervals]
+result_list_forward = list(zip(Intervals, interpolated_rates_forward))
+forward_rates = [(interval, float(rate)) for interval, rate in result_list_forward]
+
+
+#print(Discount_factor_M)
+print(Discount_factor_M[-5:])
