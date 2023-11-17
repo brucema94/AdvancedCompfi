6fe53e9be99e5cc0fbf2ce0320b856724468d87c
brucema94 <49718928+brucema94@users.noreply.github.com>
2023-11-17 18:12:05 +0100

    Add files via upload

--- SwapPricerV1.py
+++ SwapPricerV1.py
@@ -7,7 +7,7 @@ from scipy import interpolate
 # Parameters
 dt = 0.01
 num_paths = 1
-monte_carlo_amount = 1000
+monte_carlo_amount = 10
 maturity=2
 vol=0.5
 # initial interest rate is set to 0 for now. 
@@ -30,8 +30,9 @@ timeGrid = np.linspace(0, maturity_grid[0], num = 1+int(maturity_grid[0]/dt))
 
 ###############################################################################
 #Here we load in the data. The filepath is of course different per user
-loaded_data_discount = np.load('C:/Users/Trist/Master/Jaar 2/Semester 1/CompFi/CompFi/std_tenor_eonia.npy', allow_pickle=True)
-loaded_data_forward = np.load('C:/Users/Trist/Master/Jaar 2/Semester 1/CompFi/CompFi/std_tenor_eur3m.npy', allow_pickle=True)
+
+loaded_data_discount = np.load('C:/Users/bruce/Desktop/Compfi/std_tenor_eonia.npy', allow_pickle=True)
+loaded_data_forward = np.load('C:/Users/bruce/Desktop/Compfi/std_tenor_eur3m.npy', allow_pickle=True)
 
 # Define a function for linear interpolation
 def linear_interpolation(x, x_values, y_values):
@@ -105,10 +106,15 @@ def hull_white(num_paths, vol, maturity, a, dt):
     interest_rate_paths[:, 0] = r0  # Initial interest rate
     expected_shrotrate = []
     dW = np.random.normal(0, np.sqrt(dt), (num_paths, num_steps))
-    for i in range(1,num_steps+1):
-        dR = (theta(i*dt) - a*interest_rate_paths[:,i-1])*dt + vol*math.sqrt(dt)*dW[:,i-1]
-        interest_rate_paths[:,i] = interest_rate_paths[:,i-1] + dR
-        expected_shrotrate.append(np.mean(interest_rate_paths[:,i]))
+    t = np.arange(1, num_steps + 1) * dt
+    dR = (theta(t) - a * interest_rate_paths[:, :-1]) * dt + vol * np.sqrt(dt) * dW
+    # Cumulative sum to compute interest rate paths
+    interest_rate_paths[:, 1:] = np.cumsum(dR, axis=1) + interest_rate_paths[:, :-1]
+    expected_shrotrate = np.mean(interest_rate_paths, axis=0)
+    #for i in range(1,num_steps+1):
+    #    dR = (theta(i*dt) - a*interest_rate_paths[:,i-1])*dt + vol*math.sqrt(dt)*dW[:,i-1]
+    #    interest_rate_paths[:,i] = interest_rate_paths[:,i-1] + dR
+    #    expected_shrotrate.append(np.mean(interest_rate_paths[:,i]))
     return interest_rate_paths, expected_shrotrate
 
 # Retrieve the value of P(s,t) based on r(s) from Hull-White
@@ -124,41 +130,31 @@ def f(s, p_grid):
 
 # Now we have functions to determine SWAP value/NPV/EPE
 
+def find_closest_greater_index(t, T):
+    # Get the index of the closest value in T that is greater than t
+    closest_greater_index = min(range(len(T)), key=lambda i: T[i] if T[i] > t else float('inf'))
+    return closest_greater_index
 
 # Function to compute net present value for any t
 # given an interest path
-def NPV(t, interest_path):
+def NPV(t, short_rate):
     # First, simulate the short rate and compute the fixed rate
     fixed_grid = []
     p_t_T_grid = []
-    sample_paths, short_rate = hull_white(num_paths, vol, maturity_grid[-1], a, dt)
-    for T in maturity_grid:
-        fixed_grid.append(P(0, T, short_rate, p_grid[int(0/dt)], p_grid[int(T/dt)], f(0, p_grid)))
-    fixed = (fixed_grid[0] - fixed_grid[-1])/(Coupon_frequency*(sum(fixed_grid) - fixed_grid[0]))
-    # We make a case distinction
+    #short_rate = hull_white(num_paths, vol, maturity_grid[-1], a, dt)[1]
+
     upcoming_maturities = [T for T in maturity_grid if T > t]
-    # This is the case t < t0
-    if t < maturity_grid[0]:
-        for T in maturity_grid: 
+    if len(upcoming_maturities) == 1:
+        NPV=0
+    else: 
+        for T in maturity_grid:
+            fixed_grid.append(P(0, T, short_rate, p_grid[int(0/dt)], p_grid[int(T/dt)], f(0, p_grid)))
             p_t_T_grid.append(P(t, T, short_rate, p_grid[int(t/dt)], p_grid[int(T/dt)], f(t, p_grid)))
-        temp = fixed*Coupon_frequency*(sum(p_t_T_grid) - p_t_T_grid[0])
-        return notional*(p_t_T_grid[0] - p_t_T_grid[-1] - temp)
-    # If we are at T0 or beyond, we make a slight change to the formula
-    else:
-        # If only Tm is coming up, all payouts are deterministic
-        # and our exposure is known
-        if len(upcoming_maturities) == 1:
-            return 0
-        # There are multiple maturities coming up, so there is some
-        # undeterministic exposure. The payout at the upcoming maturity is
-        # known, so we only need to consider the other payouts. For this,
-        # we can use the formula from the syllabus again
-        else: 
-            for T in upcoming_maturities:
-                p_t_T_grid.append(P(t, T, short_rate, p_grid[int(t/dt)], p_grid[int(T/dt)], f(t, p_grid)))
-            temp = fixed*Coupon_frequency*(sum(p_t_T_grid) - p_t_T_grid[0])
-            return notional*(p_t_T_grid[0] - p_t_T_grid[-1] - temp)
-            
+        fixed = (fixed_grid[0] - fixed_grid[-1])/(Coupon_frequency*(sum(fixed_grid) - fixed_grid[0]))
+        next_interval = min((i for i, T in enumerate(maturity_grid) if T > 0.25), key=lambda i: maturity_grid[i])
+        NPV = notional*(p_t_T_grid[next_interval] - p_t_T_grid[-1] - fixed*Coupon_frequency*(sum(p_t_T_grid[next_interval:-1]) - p_t_T_grid[0]))
+    
+    return NPV
 
             
 # Graph the value of a swap until time T
@@ -170,17 +166,21 @@ def graph_swap_value(N, see_all_paths):
     mean_grid = np.zeros(len(t_grid))
     # First compute N paths from 0 to T
     # where per path we use one interest rate path
+    #short_rate = hull_white(num_paths, vol, maturity, a, dt)[1]
+    #swap_grid = NPV(t_grid[:, np.newaxis], short_rate).T
+    #mean_grid = np.mean(swap_grid, axis=0)
     for i in range(N):
-        interest_path, short_rate = hull_white(num_paths, vol, maturity, a, dt)
+        short_rate = hull_white(num_paths, vol, maturity, a, dt)[1]
+        print(short_rate)
         for j in range(len(t_grid)):
-            swap_grid[i,j] = NPV(t_grid[j], interest_path)
+            swap_grid[i,j] = NPV(t_grid[j], short_rate)
+        # If you want to see all paths
+        if see_all_paths:
+            plt.plot(t_grid, swap_grid[i, :])
     # Next compute Monte Carlo average
     for k in range(len(t_grid)):
         mean_grid[k] = np.mean(swap_grid[:, k])
-    # If you want to see all paths
-    if see_all_paths:
-        for i in range(N):
-            plt.plot(t_grid, swap_grid[i, :])
+        
     # In any case we graph the average
     plt.plot(t_grid, mean_grid, label='Empirical average', color='0')
     plt.xlabel('Time (t)')
@@ -189,80 +189,6 @@ def graph_swap_value(N, see_all_paths):
     plt.xlim(0, maturity)
     plt.show()
     
-
-
-# And here we have functions to determine SWAPTION value
-
-# Function to determine the value of the option of a swap
-# for all t < T0
-def swaption_value(maturity_grid, notional):
-    # First compute the "par rate" and the stochastic short rate
-    t_grid = np.linspace(0, maturity_grid[0]-dt, num = int(Coupon_frequency/dt))
-    sample_paths, short_rate = hull_white(num_paths, vol, maturity_grid[-1], a, dt)
-    new_short_rate = [short_rate[i] for i in range(len(t_grid))]
-    fixed_grid = []
-    p_t_T_grid = np.zeros((len(t_grid), len(maturity_grid)))
-    value_grid = np.zeros(len(t_grid))
-    for i in range(len(maturity_grid)): 
-        fixed_grid.append(P(0, maturity_grid[i], short_rate, p_grid[int(0/dt)], p_grid[int(maturity_grid[i]/dt)], f(0, p_grid)))
-        for j in range(len(t_grid)):
-            p_t_T_grid[j, i] = P(t_grid[j], maturity_grid[i], short_rate, p_grid[int(t_grid[j]/dt)], p_grid[int(maturity_grid[i]/dt)], f(t_grid[j], p_grid))
-    K = (fixed_grid[0] - fixed_grid[-1])/(Coupon_frequency*(sum(fixed_grid) - fixed_grid[0]))
-    for i in range(len(t_grid)):
-        s = (p_t_T_grid[i,0] - p_t_T_grid[i,-1])/(Coupon_frequency*(sum(p_t_T_grid[i]) - p_t_T_grid[i,0]))
-        annuity = Coupon_frequency*(sum(p_t_T_grid[i]) - p_t_T_grid[i,0])
-        value_grid[i] = notional*annuity*max(s-K, 0)
-        #value_grid[i] = notional*annuity*(s-K)
-    return value_grid, short_rate, new_short_rate
-
-def monte_carlo_swaption(maturity_grid, notional, monte_carlo_amount):
-    t_grid = np.linspace(0, maturity_grid[0]-dt, num = int(Coupon_frequency/dt))
-    M = len(t_grid)
-    value = np.zeros((monte_carlo_amount, M))
-    interest_paths = np.zeros((monte_carlo_amount, M))
-    for n in range(monte_carlo_amount):
-        value[n], x, interest_paths[n] = swaption_value(maturity_grid, notional)
-    # Now take an empirical average
-    mean_value = np.zeros(M)
-    mean_interest = np.zeros(M)
-    for i in range(M):
-        mean_value[i] = np.mean(value[:, i])
-        mean_interest[i] = np.mean(interest_paths[:, i])
-    return value, mean_value, t_grid, interest_paths, mean_interest
-
-
-
-# A function to graph the swaption value
-def graph_swaption_value(show_interest_rate):
-    value, mean, t_grid_swaption, interest_paths, mean_interest = monte_carlo_swaption(maturity_grid, notional, monte_carlo_amount)
-    #This is the option for showing the swaption value
-    if not show_interest_rate:
-        for n in range(monte_carlo_amount):
-            plt.plot(t_grid_swaption, value[n, :])
-        plt.plot(t_grid_swaption, mean, label='Empirical average', color='0')
-        plt.xlabel('Time (t)')
-        plt.ylabel('Value of the swaption')
-
-        plt.xlim(0, maturity_grid[0])
-        plt.title('Simulation of swaption values')
-        plt.legend()
-        plt.show()
-    #And this the option for the interest rate paths
-    if show_interest_rate:
-        for n in range(monte_carlo_amount):
-            plt.plot(t_grid_swaption, interest_paths[n, :])
-        plt.plot(t_grid_swaption, mean_interest, label='Empirical average', color='0')
-        
-        plt.xlabel('Time (t)')
-        plt.ylabel('Interest rate')
-        plt.xlim(0, maturity_grid[0])
-        
-        plt.title('Simulation of interest rate values using Hull-White')
-        plt.legend()
-        plt.show()
-
-
-###############################################################################
 
 # Now we can call on functions to e.g. graph the value of a swap(tion)
 
